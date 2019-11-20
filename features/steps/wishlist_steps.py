03fe1ff7688fc656e1f2188572a43a4cee076cdb
lombardero <manuel.lombardero.botey@gmail.com>
2019-11-20 21:44:49 +0000

    add list all products BDD

--- features/steps/wishlist_steps.py
+++ features/steps/wishlist_steps.py
@@ -88,7 +88,7 @@ def step_impl(context):
         context.resp = requests.post(create_url, data=payload, headers=headers)
         expect(context.resp.status_code).to_equal(201)
 
-@given('the following products on each respective wishlist')
+@given('the following products on the first wishlist')
 def step_impl(context):
     """ Add one product on each Wishlist """
     headers = {'Content-Type': 'application/json'}
@@ -97,7 +97,6 @@ def step_impl(context):
     for row in context.table:
         context.resp = requests.get(wishlists_url)
         wishlist_id = json.loads(context.resp.text)[id_i]['id']
-        id_i += 1
         data = {
             "product_id": int(row['product_id']),
             "product_name": row['product_name']
@@ -251,15 +250,15 @@ def step_impl(context, old_element_name, new_element_name):
     element.send_keys(new_element_name)
 
 
-@when('I check the "{wishilst_order}" wishlist')
+@when('I check the items on the first wishlist')
 def step_impl(context, wishlist_order):
     """
     Gets all items from the nth wishlist on the database
     """
-    translator_text_num= {'first':1, 'second':2, 'third':3}
-    wishlist_order = translator_text_num[wishlist_order]
-    headers = {'Content-Type': 'application/json'}
     wishlists_url = context.base_url + '/wishlists'
     context.resp = requests.get(wishlists_url)
+    wishlist_id = json.loads(context.resp.text)[0]['id']
+    print(wishlist_id)
+    context.resp = requests.get(wishlists_url)
     wishlist_id = json.loads(context.resp.text)[wishlist_order]['id']
-    context.resp = requests.get(wishlists_url + '/{}/items'.format(wishlist_id))
+    context.resp = requests.get(context.base_url + 'wishlists/{}/items'.format(wishlist_id))
